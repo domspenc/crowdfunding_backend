8e6a8768272464f81aa2362d0c161e5a1992c4c8
domspenc <dommspencer@gmail.com>
2024-09-24 18:34:57 +0800

    added code to serializers.py and views.py

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -14,54 +14,56 @@ class ProjectList(APIView):
 
 
 
-# You can either use a class or a function to create views in Django
 
-# 'index' is the function name used to label the default page in an app
-# The below are FBVS (function-based views)
-# Eg: 
-def index(request):
-    return render(request, 'index.html')
-# The above code is doing the following things:
-# The index function takes in a 'request' object as the argument (this is all the information from the user),
-# then we return back the results of the 'render' command, which takes two parameters; our 'request' and the name of the html file (index.html)
 
-# You create different views for the different pages on your site, for example:
-def about(request):
-    return render(request, 'about.html')
+# # You can either use a class or a function to create views in Django
 
-# The html file it's going to display is known (in Django terms) as a 'template', which will be static, but we can make dynamic!
-# You would start by creating a templates folder within the 'projects' folder, which will hold the html templates (but maybe wait to do this in class!)
+# # 'index' is the function name used to label the default page in an app
+# # The below are FBVS (function-based views)
+# # Eg: 
+# def index(request):
+#     return render(request, 'index.html')
+# # The above code is doing the following things:
+# # The index function takes in a 'request' object as the argument (this is all the information from the user),
+# # then we return back the results of the 'render' command, which takes two parameters; our 'request' and the name of the html file (index.html)
 
-# Routing (Wiring up Views)
-# Tells the web framework "Hey, if a user goes to this particular url or performs this particular action, *this* is now the code that I want you to excecute in response to this"
-# The way that django does this is by using a urlConf file 
-# This is a collection of paths (or routes on other web frameworks!) that are saying ("Hey, when a user goes to this path, this is where I want you to send that request")
+# # You create different views for the different pages on your site, for example:
+# def about(request):
+#     return render(request, 'about.html')
 
-# The global file for this is inside the project settings folder ('crowdfunding'), inside the urls.py file
-# urlpatterns is the global variable for this entire project
-# we use this url.py file to tell djang o where all of our urlConfs are set up so it knows wat to do
+# # The html file it's going to display is known (in Django terms) as a 'template', which will be static, but we can make dynamic!
+# # You would start by creating a templates folder within the 'projects' folder, which will hold the html templates (but maybe wait to do this in class!)
 
-# inside the app settings folder ('projects') you can add a url.py file that will be specific to the app (not global)
-# here, i need to import a library so i can start setting up my paths
-# you import it using [ from django.urls import path ]
-# then you create the urlpatterns list (this list is our urlConf!):
-# each path will look for 2 things:
-# 1. the route (this indicates where the user is going inside our application; this can be left blank indicating a default path)
-# 2. code that points to the function we want to call (located inside our views.py file!)
-# so the working code will look like this:
-urlpatterns = [
-    path('', views.index, name='index')
-]
-# you can also give it a name, which is the final parameter above. This is super helpful to prevent having to hardcode in a path, you can just look it up using the name!
-# you can add all of your paths to your urlConf, eg:
-urlpatterns = [
-    path('', views.index, name='index')
-    path('about', views.about, name='about')
-]
+# # Routing (Wiring up Views)
+# # Tells the web framework "Hey, if a user goes to this particular url or performs this particular action, *this* is now the code that I want you to excecute in response to this"
+# # The way that django does this is by using a urlConf file 
+# # This is a collection of paths (or routes on other web frameworks!) that are saying ("Hey, when a user goes to this path, this is where I want you to send that request")
 
-# Now, we need to register this information globally, so we go back into the project settings folder ('crowdfunding'), then into the existing urls.py file, and we add the new paths in
-# we do this by importing one more library called 'include'
-# this can be added to the end of the [ from django.urls import path, include ] line at the top of the file
-# 'include' will allow us to add the urlConf from the app into the global crowdfunding url.py file
-# we do this with one line of code:
-path('', include('projects.urls'))
+# # The global file for this is inside the project settings folder ('crowdfunding'), inside the urls.py file
+# # urlpatterns is the global variable for this entire project
+# # we use this url.py file to tell djang o where all of our urlConfs are set up so it knows wat to do
+
+# # inside the app settings folder ('projects') you can add a url.py file that will be specific to the app (not global)
+# # here, i need to import a library so i can start setting up my paths
+# # you import it using [ from django.urls import path ]
+# # then you create the urlpatterns list (this list is our urlConf!):
+# # each path will look for 2 things:
+# # 1. the route (this indicates where the user is going inside our application; this can be left blank indicating a default path)
+# # 2. code that points to the function we want to call (located inside our views.py file!)
+# # so the working code will look like this:
+# urlpatterns = [
+#     path('', views.index, name='index')
+# ]
+# # you can also give it a name, which is the final parameter above. This is super helpful to prevent having to hardcode in a path, you can just look it up using the name!
+# # you can add all of your paths to your urlConf, eg:
+# urlpatterns = [
+#     path('', views.index, name='index')
+#     path('about', views.about, name='about')
+# ]
+
+# # Now, we need to register this information globally, so we go back into the project settings folder ('crowdfunding'), then into the existing urls.py file, and we add the new paths in
+# # we do this by importing one more library called 'include'
+# # this can be added to the end of the [ from django.urls import path, include ] line at the top of the file
+# # 'include' will allow us to add the urlConf from the app into the global crowdfunding url.py file
+# # we do this with one line of code:
+# path('', include('projects.urls'))
